7fbb7219019b54d0c949bac44e5094fb13ba45d6
ThePlagueDoctor <NguyenZachary619@gmail.com>
2026-03-21 18:17:33 -0500

    changes to shop.html and shop.js

--- shop.js
+++ shop.js
@@ -1,18 +1,45 @@
 
-function item(name, price)
-{
-    this.name = name;
-    this.price = setCurrencyCookie(price);
 
+let playerMoney = setCurrencyCookie(walletAmount);
+
+function shopping(n)
+{
+    playerMoney.subtractCurrency(shopObj[n].getitemCost);
+    let playerMoney = getCurrencyCookie();
+    updateDisplay();
+    if(shopType =="Cosmetics")
+    {
+
+    }
+
+    if(shopType =="Consumables")
+    {
+        
+    }
+}
+function addMoney(bucks)
+{
+    playerMoney.addCurrency(bucks)
 }
 
+//list of items in the shop
+var shopObj = [
+    {
+        itemCost : setCurrencyCookie(100),
+        itemName : 'Top Hat'
+    },      
+    {
+        shopType : "Consumables",
+        itemCost : setCurrencyCookie(10),
+        itemName : 'milk'
+    },
+    {
+        shopType : "Consumables",
+        itemCost : setCurrencyCookie(5),
+        itemName : 'cat feed'
+    }
+]       
 
-function buy(amount)
-{
-
-}
-
-function sell()
-{
-
-}
+for (i =0 ; i < shopObj.length; i++)
+for (property in shopObj[i]) 
+    alert( 'shop ' + i + " 's property is " + property +  " and it's value is " + shopObj[i][property])
