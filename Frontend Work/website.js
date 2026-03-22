25a29d628227ab8f7aed67eb5535d4319cad8584
ThePlagueDoctor <143561306+ZacharyNguyen001@users.noreply.github.com>
2026-03-22 11:31:10 -0500

    fix functionality pt 2

--- Frontend Work/website.js
+++ Frontend Work/website.js
@@ -62,11 +62,9 @@ var shopObj = [
 //the function to buy stuff from the Prawn Shop
 function shopping(n) {
     let item = shopObj[n];
-    let bought = item.subtractCurrency(item.itemCost);
+
+    subtractCurrency(item.itemCost);
     return getCurrencyCookie();
 }
 
-
-for (i = 0; i < shopObj.length; i++)
-    for (property in shopObj[i])
-        alert('shop ' + i + " 's property is " + property + " and it's value is " + shopObj[i][property])
+// addCurrency(500);
